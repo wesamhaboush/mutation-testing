5f3a52d655a7529d6c075443922a3150d00c67f4
Wesam Haboush <wesam.haboush@gmail.com>
2016-02-11 21:28:10 -0800

    added some log for cancellation

--- java-mutation-testing/src/main/java/com/codebreeze/mutation/hotel/Hotel.java
+++ java-mutation-testing/src/main/java/com/codebreeze/mutation/hotel/Hotel.java
@@ -1,8 +1,10 @@
 package com.codebreeze.mutation.hotel;
 
 import java.time.LocalDateTime;
+import java.util.Optional;
 import java.util.Set;
 import java.util.function.Predicate;
+import java.util.stream.IntStream;
 
 import static com.google.common.base.Preconditions.checkState;
 import static com.google.common.collect.Sets.newHashSet;
@@ -10,6 +12,15 @@ import static com.google.common.collect.Sets.newHashSet;
 public class Hotel {
     private final Set<Room> availableRooms = newHashSet();
     private final Set<Reservation> reservations = newHashSet();
+
+    public Hotel(final int roomCount){
+        checkState(roomCount > 0, "cannot have a hotel with negative or zero room count [%d]", roomCount);
+        IntStream.range(0, roomCount).forEach(i -> availableRooms.add(createRoom(i)));
+    }
+
+    private Room createRoom(int i) {
+        return new Room(String.valueOf(i));
+    }
 
     public Reservation reserve(final String roomNumber,
                                final LocalDateTime from,
@@ -22,6 +33,11 @@ public class Hotel {
     }
 
     public void cancel(final String reservationNumber){
+        final Optional<Reservation> reservation = reservations
+                .stream()
+                .filter(hasReservationNumber(reservationNumber))
+                .findFirst();
+        reservation.ifPresent(r -> availableRooms.add(new Room(r.getRoomNumber())));
         final boolean removed = reservations.removeIf(hasReservationNumber(reservationNumber));
         checkState(removed, "reservation not found [%s]", reservationNumber);
     }
